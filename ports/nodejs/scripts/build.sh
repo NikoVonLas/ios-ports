449cd0e215699e14bb73affa375120d5b48169b9
Denis Nikolas <d.nikolas@xsolla.com>
2026-08-10 14:19:56 +0500

    Wrap Node host compilers for Apple linker

--- ports/nodejs/scripts/build.sh
+++ ports/nodejs/scripts/build.sh
@@ -34,8 +34,8 @@ cd "$source_root"
 make distclean >/dev/null 2>&1 || true
 
 export IOS_DEPLOYMENT_TARGET
-export CC_host="$(xcrun -f clang)"
-export CXX_host="$(xcrun -f clang++)"
+export CC_host="$project_root/scripts/macos-clang"
+export CXX_host="$project_root/scripts/macos-clang++"
 export CFLAGS_host="-isysroot $host_sdk_root"
 export CXXFLAGS_host="-isysroot $host_sdk_root -std=gnu++20"
 export LDFLAGS_host="-isysroot $host_sdk_root"
